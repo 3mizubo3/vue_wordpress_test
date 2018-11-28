76f1f37d7d19224007b571b5b19ca2c7b8844d67
mizuho <mizuho@birdman.ne.jp>
2018-11-28 22:57:27 +0900

    Test

---
+++ vue_wp_test/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+<<<<<<< HEAD
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+=======
+    <img src="./assets/logo.png">
+    <router-view/>
+>>>>>>> origin/master
+  </div>
+</template>
+
+<script>
+<<<<<<< HEAD
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+=======
+export default {
+  name: 'App'
+>>>>>>> origin/master
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue_wp_test/src/components/Service.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <router-link to="/">ホーム </router-link>
+    <router-link to="/service">サービス </router-link>
+    <router-link to="/news">ニュース </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'サービスページ'
+    }
+  }
+}
+</script>
+
+<style scoped>
+    h1, h2 {
+    font-weight: normal;
+    }
+    a {
+      color: #42b983;
+    }
+</style>
